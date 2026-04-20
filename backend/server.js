0ad44b4c50a35a386cce744697ff4ec5a19d3a67
yassinbenelhajlahsen <yassinbenelhajlahsen@gmail.com>
2026-04-20 10:34:27 -0400

    refactor: remove session middleware and add .env.example file

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 // dependencies
 import express from 'express';
 import cors from 'cors';
-import session from 'express-session';
 import 'dotenv/config';
 
 import homePageRoutes from './routes/homePageRoutes.js'
@@ -19,16 +18,6 @@ app.use(cors({
 }))
 
 app.use(express.json()) // handling json data
-
-app.use(session({
-    secret: process.env.SESSION_SECRET, // crypotgraphic reasons:                                                                   https://expressjs.com/en/resources/middleware/session.html#:~:text=0%2E3%2E0-,secret
-    resave: false, // saves user input regardless of whether there was any or not:                                                  https://expressjs.com/en/resources/middleware/session.html#:~:text=resave,-Forces
-    saveUninitialized: false, //                                                                                                    https://expressjs.com/en/resources/middleware/session.html#:~:text=saveUninitialized,-Forces
-    cookie: { //                                                                                                                    https://expressjs.com/en/resources/middleware/session.html#:~:text=req%2Esession%2Ecookie,-Each
-        secure: false, // we do not have an https conntection                                                                       https://expressjs.com/en/resources/middleware/session.html#:~:text=cookie%2Esecure
-        sameSite: 'lax' // permitting it for "safe" top-level navigations (like GET links) that change the URL in the address bar.  https://expressjs.com/en/resources/middleware/session.html#:~:text=cookie%2EsameSite
-    }
-}))
 
 app.use('/products', homePageRoutes);
 

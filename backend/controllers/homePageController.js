f62f30ab27bcb2850a92da98c6a7922f194278ab
yassinbenelhajlahsen <yassinbenelhajlahsen@gmail.com>
2026-04-20 13:02:40 -0400

    refactor(backend): remove unnecessary console log

--- backend/controllers/homePageController.js
+++ backend/controllers/homePageController.js
@@ -4,6 +4,5 @@ import dummyData from '../db/dummyData.json' with {type: 'json'};
 // this function is currently returning dummy data
 export async function getProducts(req,res){
     // we prob want to instert an access token to our supabase db
-    console.log(JSON.stringify(dummyData)); // have to convert to string before sending it as response
     res.send(dummyData); // send it 
 }

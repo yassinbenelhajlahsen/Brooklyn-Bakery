35b1e752a36cddd3b226b205c031045e207a88a2
yassinbenelhajlahsen <yassinbenelhajlahsen@gmail.com>
2026-04-20 13:02:40 -0400

    refactor(backend): remove unnecessary console log

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,4 @@
 export async function createOrder(req, res) {
     const lineItems = Object.keys(req.body ?? {}).length;
-    console.log(`[orders] user=${req.user.id} lineItems=${lineItems}`);
     res.status(200).json({ ok: true, received: req.body });
 }

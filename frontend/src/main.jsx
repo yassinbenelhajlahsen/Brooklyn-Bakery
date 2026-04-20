9aeb69344be9b2d2d0453ae609db4781fe722cf4
yassinbenelhajlahsen <yassinbenelhajlahsen@gmail.com>
2026-04-20 10:21:04 -0400

    refactor: clean up frontend structure
    - Move HomePage.jsx from components/ to pages/
    - Make App.jsx the top-level component rendering HomePage
    - Reduce main.jsx to mounting <App />
    - Remove Vite/React demo boilerplate (App.css, react.svg, vite.svg)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import HomePage from './components/HomePage.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HomePage/>
+    <App />
   </StrictMode>,
 )

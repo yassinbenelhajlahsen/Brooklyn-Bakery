03ce0a6651f15aa6567f9104f52cec8c3108cda6
siMoon646 <simontang671@gmail.com>
2026-05-06 22:59:22 -0400

    adjusted positioning of UI elements

--- frontend/src/pages/EarnPage.jsx
+++ frontend/src/pages/EarnPage.jsx
@@ -2,7 +2,7 @@ import CookieClicker from '../components/CookieClicker.jsx'
 
 export default function EarnPage() {
   return (
-    <section className="flex items-center justify-center px-8 py-10 max-sm:px-4 min-h-full w-full">
+    <section className="relative flex items-center justify-center px-8 py-10 max-sm:px-4 min-h-full w-full">
       <CookieClicker />
     </section>
   )

ba712183854448f35b9e4ec682a805683afe2553
e <zus@es-MacBook-Pro.local>
2026-05-11 23:06:23 -0400

    added cursor per upgrade

--- backend/controllers/cookieUpgradesController.js
+++ backend/controllers/cookieUpgradesController.js
@@ -4,6 +4,11 @@ import { httpError } from '../lib/httpError.js';
 import { normalizeAddressInput } from '../lib/address.js';
 import { Prisma } from '@prisma/client';
 
+const upgrades = {
+    15 : "one_half_points",
+    25 : "double_points",
+    50 : "triple_points"
+}
 
 export const getUserPoints = async (req, res) => {
 
@@ -18,3 +23,27 @@ export const getUserPoints = async (req, res) => {
 
     res.json(user.balance);
 };
+
+
+export const applyUpgrades = async (req, res) => {
+
+    const user = await prisma.user.findFirst({
+        where: { id: req.params.id }
+    });
+
+    console.log(user);
+
+    let highestUpgrade = ""
+
+    for(const key in upgrades){
+        if (user.balance >= key)
+            highestUpgrade = upgrades[key]
+   }
+
+    console.log(highestUpgrade)
+
+    res.json(highestUpgrade)
+
+}
+
+
